773a3ba8eb5b3799c1debf18bd66fdb04cb9cb6c
crnbrdrck <broderickciaran@gmail.com>
2017-03-02 18:55:42 +0000

    Added testing to the Server module
    Added argument parsing to the server module to allow a test version to be run with the -t flag that will print out every event and always buy

--- Server/__main__.py
+++ Server/__main__.py
@@ -1,4 +1,5 @@
 from sys import exit, stderr
+from argparse import ArgumentParser
 try:
     from .Server import Server
 except SystemError:
@@ -9,12 +10,28 @@ except SystemError:
 Allows for running the code using python3 -m Server
 """
 
-# Set up a Server instance
-server = Server()
+parser = ArgumentParser(
+    prog="Monopoly.Server",
+    usage="python3 -m Server [-h] [-t]",
+    description="%(prog)s: Server of the Monopoly game written by Super Confused"
+)
+parser.add_argument(
+    '-t', '--test',
+    help='Runs a test of the server',
+    action='store_true',
+)
 
-# Allow for Keyboard interrupts
-try:
-    server.serve()
-except:
-    server.close()
+# Set up a Server instance
+args = parser.parse_args()
+if args.test:
+    from .TestClient import run_test
+    run_test()
+else:
+    server = Server()
+
+    # Allow for Keyboard interrupts
+    try:
+        server.serve()
+    except KeyboardInterrupt:
+        server.close()
 
