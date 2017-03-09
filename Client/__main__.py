9e29b777e0fe17a3e13eca1c290d11e1df3e2f78
crnbrdrck <broderickciaran@gmail.com>
2017-03-09 12:05:35 +0000

    Eggsecutable

--- Client/__main__.py
+++ Client/__main__.py
@@ -10,56 +10,60 @@ except SystemError:
 """
 Allows for running the code using python3 -m Client
 """
-gui = Main()
-window = Tk()
-window.title("Monopoly")
-host = StringVar()
-host.set("127.0.0.1")
-name = StringVar()
-name.set("Guest")
-password = StringVar()
+def main():
+    gui = Main()
+    window = Tk()
+    window.title("Monopoly")
+    host = StringVar()
+    host.set("127.0.0.1")
+    name = StringVar()
+    name.set("Guest")
+    password = StringVar()
 
 
-def create():
-    password_text = password.get()
-    password_text = password_text if password_text != '' else None
-    gui.create(host.get(), name.get(), password_text)
-    run()
+    def create():
+        password_text = password.get()
+        password_text = password_text if password_text != '' else None
+        gui.create(host.get(), name.get(), password_text)
+        run()
 
 
-def join():
-    password_text = password.get()
-    password_text = password_text if password_text != '' else None
-    gui.join(host.get(), name.get(), password_text)
-    run()
+    def join():
+        password_text = password.get()
+        password_text = password_text if password_text != '' else None
+        gui.join(host.get(), name.get(), password_text)
+        run()
 
 
-def poll():
-    print("Polling the network for games")
-    servers = gui.poll()
-    if not servers:
-        print("No Servers running")
-    else:
-        for server in servers:
-            print(server)
-    exit(0)
+    def poll():
+        print("Polling the network for games")
+        servers = gui.poll()
+        if not servers:
+            print("No Servers running")
+        else:
+            for server in servers:
+                print(server)
+        exit(0)
 
 
-def run():
-    # Now run the game
-    window.destroy()
-    gui.init_display()
-    gui.run()
+    def run():
+        # Now run the game
+        window.destroy()
+        gui.init_display()
+        gui.run()
 
-f = LabelFrame(window, text="Host IP")
-Entry(f, textvariable=host).pack(side=TOP, fill=BOTH, expand=1)
-f.pack(side=TOP, fill=BOTH, expand=1)
-f = LabelFrame(window, text="Username")
-Entry(f, textvariable=name).pack(side=TOP, fill=BOTH, expand=1)
-f.pack(side=TOP, fill=BOTH, expand=1)
-f = LabelFrame(window, text="Password")
-Entry(f, textvariable=password).pack(side=TOP, fill=BOTH, expand=1)
-f.pack(side=TOP, fill=BOTH, expand=1)
-Button(window, text="Create", command=create).pack(side=TOP, fill=BOTH, expand=1)
-Button(window, text="Join", command=join).pack(side=TOP, fill=BOTH, expand=1)
-window.mainloop()
+    f = LabelFrame(window, text="Host IP")
+    Entry(f, textvariable=host).pack(side=TOP, fill=BOTH, expand=1)
+    f.pack(side=TOP, fill=BOTH, expand=1)
+    f = LabelFrame(window, text="Username")
+    Entry(f, textvariable=name).pack(side=TOP, fill=BOTH, expand=1)
+    f.pack(side=TOP, fill=BOTH, expand=1)
+    f = LabelFrame(window, text="Password")
+    Entry(f, textvariable=password).pack(side=TOP, fill=BOTH, expand=1)
+    f.pack(side=TOP, fill=BOTH, expand=1)
+    Button(window, text="Create", command=create).pack(side=TOP, fill=BOTH, expand=1)
+    Button(window, text="Join", command=join).pack(side=TOP, fill=BOTH, expand=1)
+    window.mainloop()
+
+if __name__ == '__main__':
+    main()
---
+++ client_setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="Monopoly.Client",
+    version="1.0",
+    description="Super Confused's Monopoly Client",
+    author="Super Confused",
+    packages=['Client'],
+    entry_points={
+        'setuptools.installation': [
+            'eggsecutable = Client.__main__:main',
+        ]
+    }
+)
